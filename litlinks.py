e1cfdfb1f4f47a6f9cdfb2f978ebb658af2ad910
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2019-06-20 19:14:40 +0300

    Info page added

--- litlinks.py
+++ litlinks.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
 if __name__ == '__main__':
 	app.run()
