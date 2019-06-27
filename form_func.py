7bb1f98ab3461bd16620dc4ec161e0f700c6990d
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2019-06-27 22:11:46 +0300

    Journal article form works

--- form_func.py
+++ form_func.py
@@ -1,4 +1,11 @@
-def journal_article_form(frontend_request):
-	print('JOURNAL ARTICLE - ' + str(frontend_request))
-	
-	return()
+def journal_article_form(request):
+    print('JOURNAL ARTICLE - ' + str(request))
+
+    r = request
+
+    reference_string = r['author_surname'] + ', ' + r['author_initial1'] + '.' + r['author_initial2'] + '. '
+    reference_string += r['article_name'] + ' / '
+    reference_string += r['author_initial1'] + '.' + r['author_initial2'] + '. ' + r['author_surname']
+    reference_string += '. // ' + r['journal_name'] + '. – № ' + r['journal_number'] + '.'
+
+    return(reference_string)
