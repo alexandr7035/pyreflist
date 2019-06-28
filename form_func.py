4c7effa6642fd4655ada602dde982b25158b2b11
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2019-06-28 19:30:29 +0300

    Fix: added year of publishing to journal article reference

--- form_func.py
+++ form_func.py
@@ -6,6 +6,6 @@ def journal_article_form(request):
     reference_string = r['author_surname'] + ', ' + r['author_initial1'] + '.' + r['author_initial2'] + '. '
     reference_string += r['article_name'] + ' / '
     reference_string += r['author_initial1'] + '.' + r['author_initial2'] + '. ' + r['author_surname']
-    reference_string += '. // ' + r['journal_name'] + '. – № ' + r['journal_number'] + '.'
+    reference_string += '. // ' + r['journal_name'] + '. – ' + r['publishing_year'] + '. – № ' + r['journal_number'] + '.'
 
     return(reference_string)
